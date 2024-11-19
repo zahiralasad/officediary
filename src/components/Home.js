44a37c376fef81ab8e0fe948e2dea7d7e79e29dd
Al-Asad Zahir <zahir.al-asad@scania.com>
2024-11-19 17:25:13 +0100

    started working on the app

---
+++ src/components/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Home() {
+    return (
+        <div className="text-center text-white">
+            Home page
+        </div>
+    )
+}
+export default Home;
